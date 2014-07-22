e995951742c4060ddbc1782c718aafb8efe8b4d0
Ryan Connelly <ryan@atlantacs.com>
2014-07-22 03:35:29 -0400

    updated production db

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    //db: 'mongodb://prorata:prorata1@ds027799.mongolab.com:27799/prorata-prod',
+    db: 'mongodb://prorata:prorata1@ds027799.mongolab.com:27799/prorata-prod',
     slack:{
       meandomain : 'meanio',
       meantoken : 'xoxp-2194933696-2194933708-2443321518-a2df23'
