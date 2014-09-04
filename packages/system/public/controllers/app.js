92ec19c920a59b9249e9de5fc500833b2bc1df1b
Ryan Connelly <ryan@atlantacs.com>
2014-09-04 16:13:43 -0400

    Added missing strict

--- packages/system/public/controllers/app.js
+++ packages/system/public/controllers/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 angular.module('mean.system').controller('AppController', ['$rootScope','$scope', '$location', 'Global',
   function($rootScope, $scope, $location, Global) {
     $rootScope.bodyClass = '';
